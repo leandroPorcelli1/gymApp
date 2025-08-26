10afea0d126ed6208a4bbed09855bd0d40a44a07
martin-alexis <alexismartin090@gmail.com>
2025-08-26 11:38:02 -0300

    se configuro la conexion a la db en server Neon

--- config.py
+++ config.py
@@ -1,29 +1,54 @@
 import os
+import re
+import ssl
+import asyncio
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.sql import text
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
-
-# Cargar variables de entorno
-load_dotenv()
 
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SQLALCHEMY_ENGINE_OPTIONS = {
-        'pool_pre_ping': True  # Activar el pre-ping en el pool de conexiones
+        'pool_pre_ping': True
     }
-    
-    
+    load_dotenv()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # Configuración de Google OAuth
     GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
 
-    
-    # Configuración de Turso
-    
-    #TURSO_DATABASE_URL = os.environ.get("TURSO_DATABASE_URL")
-    #TURSO_AUTH_TOKEN = os.environ.get("TURSO_DATABASE_TOKEN")
+    # 👇 Flask-SQLAlchemy necesita esta URI
+    SQLALCHEMY_DATABASE_URI = re.sub(
+        r'^postgresql:',
+        'postgresql+psycopg2:',
+        os.getenv("DATABASE_URL")
+    )
 
-    # --- NUEVA CONFIGURACIÓN ---
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_POSTGRESQL")
+    @staticmethod
+    async def async_main() -> None:
+        db_url = os.getenv('DATABASE_URL')
+
+        # Reemplazar driver para async
+        db_url = re.sub(r'^postgresql:', 'postgresql+asyncpg:', db_url)
+
+        # Limpiar parámetros extra (ej: ?sslmode=require)
+        clean_url = re.sub(r'\?(.*)', '', db_url)
+
+        # Crear contexto SSL
+        ssl_ctx = ssl.create_default_context()
+        ssl_ctx.check_hostname = True
+        ssl_ctx.verify_mode = ssl.CERT_REQUIRED
+
+        # Engine async
+        engine = create_async_engine(
+            clean_url,
+            connect_args={"ssl": ssl_ctx},
+            echo=True
+        )
+
+        async with engine.connect() as conn:
+            result = await conn.execute(text("select 'hello world'"))
+            print(result.fetchall())
+
+        await engine.dispose()
+
+

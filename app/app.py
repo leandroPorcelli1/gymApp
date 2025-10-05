02470f51a7ed1356bdba6d7f80490a82e14359ac
martin-alexis <alexismartin090@gmail.com>
2025-10-05 16:37:40 -0300

    se borro endpoints innecesarios d usuarios, rutinas_completas y entrenamiento_realizados

--- app/app.py
+++ app/app.py
@@ -10,23 +10,13 @@ db.init_app(app)
 
 # Importar y registrar blueprints
 from rutas.routes_usuarios import usuarios_bp
-#from routes_rutinas import rutinas_bp
 from rutas.routes_ejercicios import ejercicios_bp
-#from routes_series import series_bp
-#from routes_entrenamientos import entrenamientos_bp
 from rutas.routes_entrenamientos_realizados import entrenamientos_realizados_bp
-#from routes_series_realizadas import series_realizadas_bp
-#from routes_nivel_rutinas import nivel_rutinas_bp
 from rutas.routes_rutinas_completas import rutinas_completas_bp
 
 app.register_blueprint(usuarios_bp)
-#app.register_blueprint(rutinas_bp)
 app.register_blueprint(ejercicios_bp)
-#app.register_blueprint(series_bp)
-#app.register_blueprint(entrenamientos_bp)
 app.register_blueprint(entrenamientos_realizados_bp)
-#app.register_blueprint(series_realizadas_bp)
-#app.register_blueprint(nivel_rutinas_bp)
 app.register_blueprint(rutinas_completas_bp)
 
 if __name__ == '__main__':

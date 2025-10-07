cb4fccf9f03882c31819a253180ffec8e1d68f89
martin-alexis <alexismartin090@gmail.com>
2025-10-07 07:52:56 -0300

    se agrego el modelo ejercicios_usuario

--- modelos/models.py
+++ modelos/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
     id_usuarios = db.Column(db.Integer, primary_key=True)
@@ -16,6 +17,22 @@ class Usuario(db.Model):
 
     rutinas = db.relationship('Rutina', backref='usuario', cascade="all, delete-orphan")
     entrenamientos = db.relationship('Entrenamiento', backref='usuario', cascade="all, delete-orphan")
+    ejercicios_usuario = db.relationship("EjercicioUsuario", back_populates="usuario")
+
+
+
+class EjercicioUsuario(db.Model):
+    __tablename__ = "ejercicios_usuario"
+
+    id_ejercicios_usuario = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    descripcion = db.Column(db.Text)
+    video_url = db.Column(db.Text)
+    usuarios_id = db.Column(db.Integer, db.ForeignKey("usuarios.id_usuarios"), nullable=False)
+
+    # Relación inversa
+    usuario = db.relationship("Usuario", back_populates="ejercicios_usuario")
+
 
 class Rutina(db.Model):
     __tablename__ = 'rutinas'
@@ -23,10 +40,12 @@ class Rutina(db.Model):
     nombre = db.Column(db.String(100), nullable=False, unique=True)
     descripcion = db.Column(db.String(500))
     usuarios_id = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuarios', ondelete='CASCADE'), nullable=False)
-    nivel_rutinas_id = db.Column(db.Integer, db.ForeignKey('nivel_rutinas.id_nivel_rutinas', ondelete='RESTRICT'), nullable=False)
+    nivel_rutinas_id = db.Column(db.Integer, db.ForeignKey('nivel_rutinas.id_nivel_rutinas', ondelete='RESTRICT'),
+                                 nullable=False)
 
     ejercicios = db.relationship('Ejercicio', backref='rutina', cascade="all, delete-orphan")
     entrenamientos = db.relationship('Entrenamiento', backref='rutina', cascade="all, delete-orphan")
+
 
 class NivelRutina(db.Model):
     __tablename__ = 'nivel_rutinas'
@@ -35,22 +54,26 @@ class NivelRutina(db.Model):
 
     rutinas = db.relationship('Rutina', backref='nivel', cascade="all, delete-orphan")
 
+
 class EjercicioBase(db.Model):
     __tablename__ = 'ejercicios_base'
     id_ejercicios_base = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String, nullable=False)
     descripcion = db.Column(db.String)
     video_url = db.Column(db.String, nullable=True)
-    
+
     ejercicios = db.relationship('Ejercicio', backref='ejercicio_base', cascade="all, delete-orphan")
+
 
 class Ejercicio(db.Model):
     __tablename__ = 'ejercicios'
     id_ejercicios = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    ejercicios_base_id = db.Column(db.Integer, db.ForeignKey('ejercicios_base.id_ejercicios_base', ondelete='CASCADE'), nullable=False)
+    ejercicios_base_id = db.Column(db.Integer, db.ForeignKey('ejercicios_base.id_ejercicios_base', ondelete='CASCADE'),
+                                   nullable=False)
     rutinas_id = db.Column(db.Integer, db.ForeignKey('rutinas.id_rutinas', ondelete='CASCADE'), nullable=False)
 
     series = db.relationship('Serie', backref='ejercicio', cascade="all, delete-orphan")
+
 
 class Entrenamiento(db.Model):
     __tablename__ = 'entrenamientos'
@@ -61,15 +84,19 @@ class Entrenamiento(db.Model):
 
     realizados = db.relationship('EntrenamientoRealizado', backref='entrenamiento', cascade="all, delete-orphan")
 
+
 class EntrenamientoRealizado(db.Model):
     __tablename__ = 'entrenamientos_realizados'
     id_entrenamientos_realizados = db.Column(db.Integer, primary_key=True)
-    entrenamientos_id = db.Column(db.Integer, db.ForeignKey('entrenamientos.id_entrenamientos', ondelete='CASCADE'), nullable=False)
+    entrenamientos_id = db.Column(db.Integer, db.ForeignKey('entrenamientos.id_entrenamientos', ondelete='CASCADE'),
+                                  nullable=False)
     ejercicios_id = db.Column(db.Integer, db.ForeignKey('ejercicios.id_ejercicios', ondelete='CASCADE'), nullable=False)
 
-    series_realizadas = db.relationship('SerieRealizada', backref='entrenamiento_realizado', cascade="all, delete-orphan")
+    series_realizadas = db.relationship('SerieRealizada', backref='entrenamiento_realizado',
+                                        cascade="all, delete-orphan")
 
     ejercicio = db.relationship('Ejercicio', backref='entrenamientos_realizados')
+
 
 class Serie(db.Model):
     __tablename__ = 'series'
@@ -78,9 +105,12 @@ class Serie(db.Model):
     peso_kg = db.Column(db.Float, nullable=False)
     ejercicios_id = db.Column(db.Integer, db.ForeignKey('ejercicios.id_ejercicios', ondelete='CASCADE'), nullable=False)
 
+
 class SerieRealizada(db.Model):
     __tablename__ = 'series_realizadas'
     id_series_realizadas = db.Column(db.Integer, primary_key=True)
-    entrenamientos_realizados_id = db.Column(db.Integer, db.ForeignKey('entrenamientos_realizados.id_entrenamientos_realizados', ondelete='CASCADE'), nullable=False)
+    entrenamientos_realizados_id = db.Column(db.Integer,
+                                             db.ForeignKey('entrenamientos_realizados.id_entrenamientos_realizados',
+                                                           ondelete='CASCADE'), nullable=False)
     repeticiones = db.Column(db.Integer, nullable=False)
     peso_kg = db.Column(db.Float, nullable=False)
